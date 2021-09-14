785d06d5d4786590a0ab9acd87552890cb84b57f
LeAnhTuan59/webApplication1 <tenlatuan1002@gmail.com>
2021-09-14 09:31:28 +0700

    Tuan

---
+++ giaiphuongtrinh/Models/LTQlDBcontext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace giaiphuongtrinh.Models
+{
+    public partial class LTQLDbContext : DbContext
+    {
+        public LTQLDbContext() : base("name=LTQLDbContext")
+        {
+        }
+        public virtual DbSet<Account> Accounts { get; set; }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+           .Property(e => e.Usename)
+           .IsUnicode(false);
+            modelBuilder.Entity<Account>().Property(e => e.Password)
+            .IsUnicode(false);
+        }
+    }
+
+}
---
+++ giaiphuongtrinh/Models/account.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BaiNgay7hang9.Models
+{
+    public class Account
+    {
+        //bắt buộc phải có 
+        [Key]
+        public string Usename { get; set; }
+        public string Password { get; set; }
+
+    }
+}
